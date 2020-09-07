b56a4e93c5d1ba71431b77133f311ddcbe68af8c
Thomas Bielich <thomas.bielich@tui.com>
2020-09-07 20:11:59 +0200

    setup static site generator "eleventy"

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const pluginSass = require("eleventy-plugin-sass");
+const markdownIt = require("markdown-it");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSass, { watch: ["src/_includes/styles/base.scss"] })
+  eleventyConfig.addPassthroughCopy({"src/_includes/scripts": "/"});
+  eleventyConfig.addShortcode("page_prefix", () => process.env.PAGE_PREFIX || "");
+
+  return {
+    dir: {
+      input: 'src/',
+      output: 'dist/'
+    },
+    passthroughFileCopy: true,
+    templateFormats: ['js', 'njk', 'md', 'css', 'html', 'yml'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk"
+  }
+}
